2e0f351e1392ecb6ce0d8e3523f25b2da6244bc2
Nuclearlolly <nuclearlolly2@gmail.com>
2025-09-23 20:20:43 -0300

    featured 6

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,9 +6,6 @@ use App\Models\Gallery\GallerySubmission;
 use App\Models\SitePage;
 use App\Services\LinkService;
 use App\Services\UserService;
-use Config;
-use Carbon\Carbon;
-use Settings;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
